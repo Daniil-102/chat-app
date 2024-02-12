0ba19fd99923b0aab6bca32d28f2847b587ff470
Daniel Kononchuk <kononchuk.daniil@knu.ua>
2024-02-12 13:16:29 +0200

    c

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,31 +1,31 @@
-import { useEffect, useState } from 'react'
-import { useSocketContext } from '../context/SocketContext'
-import { useConversation } from '../zustand/useConversation';
-import notification from '../assets/sound/notification.mp3'
-import { useSoundContext } from '../context/SoundContext';
-
 export const useListenMessages = () => {
-	const { socket } = useSocketContext();
-	const { messages, setMessages, selectedConversation  } = useConversation();
-    const { isSoundOn, toggleSound } = useSoundContext()
+    const { socket } = useSocketContext();
+    const { messages, setMessages } = useConversation();
+    const { isSoundOn, toggleSound } = useSoundContext();
 
-	useEffect(() => {
-        let sound 
-        
-		socket?.on("newMessage", (newMessage) => {
-			newMessage.shouldShake = true;
+    useEffect(() => {
+        let sound;
+
+        if (!socket) return;
+
+        const handleNewMessage = (newMessage) => {
+            newMessage.shouldShake = true;
             if (isSoundOn) {
                 sound = new Audio(notification);
                 sound.play();
             }
-			if (selectedConversation && newMessage.conversationId === selectedConversation._id) {
-                setMessages([...messages, newMessage]);
-            }
-		});
+            setMessages((prevMessages) => {
+                const updatedMessages = [...prevMessages, newMessage];
+                return updatedMessages;
+            });
+        };
 
-		return () => socket?.off("newMessage");
-	}, [socket, setMessages, messages, selectedConversation, isSoundOn]);
+        socket.on("newMessage", handleNewMessage);
 
+        return () => {
+            socket.off("newMessage", handleNewMessage);
+        };
+    }, [socket, setMessages, messages, isSoundOn]);
 
     return { isSoundOn, toggleSound };
-}
+};
